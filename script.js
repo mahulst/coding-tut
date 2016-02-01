6b2c54c605866ab271e3fddca4bd71a475a640de
mahulst <hulst.michel@gmail.com>
2016-02-01 21:34:30 +0100

    update for clarity

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/**
+ * Below this text is where we can put our code for the assignments:*/
+
+
+function add (a, b) {
+    return a + b;
+};
+
+
+
+
+
+
+
+/**
+
+ YOU CAN IGNORE TEXT BELOW THIS LINE
+ */
+
 function addInputs () {
     var value1 = parseInt(input1.value) || 0;
     var value2 = parseInt(input2.value) || 0;
@@ -8,12 +27,4 @@ function multiplyInputs () {
     var value1 = parseInt(input1.value) || 0;
     var value2 = parseInt(input2.value) || 0;
     input3.value = multiply(value1, value2);
-};
-
-/**
- * Below this text is where we can put our code for the assignments:*/
-
-
-function add (a, b) {
-    return a + b;
 };
